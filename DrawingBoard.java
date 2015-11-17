46e73287defc4ff63e6daa0a1c110405f93e5e9c
XuanyiZhu <xzhu2@uvm.edu>
2015-11-17 17:59:00 -0500

    Update DrawingBoard.java

--- DrawingBoard.java
+++ DrawingBoard.java
@@ -2,7 +2,6 @@
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 /*
@@ -25,7 +24,6 @@ public class DrawingBoard {
     private final DoubleProperty YValue = new SimpleDoubleProperty(LineY);
 
     private final Line line = new Line(0, 0, 0, 0);
-    Circle helper = new Circle(0, 0, 10);
 
     private final KrakoutBoard board;
 
@@ -38,7 +36,6 @@ public class DrawingBoard {
         line.endXProperty().bind(End_x);
         line.endYProperty().bind(YValue);
 
-        helper.centerXProperty().bind((Start_x.add(End_x)).multiply(0.5));
     }
 
     public void moreBoard(double a, double b) {
@@ -85,14 +82,6 @@ public class DrawingBoard {
      */
     public DoubleProperty getYValue() {
         return YValue;
-    }
-
-    public double getCenterX() {
-        return helper.getCenterX();
-    }
-
-    public double getCenterY() {
-        return helper.getCenterY();
     }
 
     public double getLineMidX() {
